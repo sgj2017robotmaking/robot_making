4dcb2fba56f573447d16ae552fc58f254bd9832c
tomi-ta <vrl.iwbtg@gmail.com>
2017-07-08 17:10:33 +0900

    aaaa

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour: MonoBehaviour
+{
+
+    //変数定義
+    public float flap = 1000f;
+    public float scroll = 5f;
+    float direction = 0f;
+    bool jump = false;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //キーボード操作
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = 1f;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = -1f;
+        }
+        else
+        {
+            direction = 0f;
+        }
+
+    }
+
+}
