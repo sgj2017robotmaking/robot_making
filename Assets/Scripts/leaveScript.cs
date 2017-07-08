a74b8337b788d45442ce5f83cce284939bc80846
MYCOMPUTER\koro_ <powerbox507@gmail.com>
2017-07-09 02:17:32 +0900

    hanareru

---
+++ Assets/Scripts/leaveScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leaveScript : MonoBehaviour {
+
+    private bool Switch;
+
+    void Start()
+    {
+        Switch = true;
+    }
+    void Update () {
+        
+        GameObject player = GameObject.FindWithTag("Player");
+
+        float length = Mathf.Abs(this.gameObject.transform.position.x * 0.8f - player.transform.position.x);
+        float speed = 0.1f;
+        float step = Time.deltaTime * speed;
+        step = speed / length* -1.0f;
+        if (Switch == true)
+        {
+            if (Input.GetKey("q"))
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.transform.position, step);
+                Debug.Log("q");
+            }
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("!");
+
+            this.gameObject.GetComponent<leaveScript>().enabled = false;
+        }
+
+    }
+}
